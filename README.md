e8ac587e0cb8641c02b61eec0e644b458cdd1720
codeeffect <steve@codeeffect.co.uk>
2013-05-17 21:04:36 +0100

    First commit of FooLime

---
+++ README.md
@@ -0,0 +1,55 @@
+# FooLime #
+
+A plugin for both Sublime Text 2 and 3 that allows you to control foobar2000 from
+the quick panel.
+
+## Details ##
+
+This one is more of a hobby project, just playing with Sublime and not really 
+intended for general use. That being said however, if you do install it and find 
+it useful please do let me know if you have any feature requests, comments etc :)
+
+## Manual installation ##
+
+At present the plugin is not in package control so you will need to install manually.
+
+### Using GIT (recommended): ###
+Go to the Packages directory (`Preferences` / `Browse Packages…`). Then clone this
+repository:
+
+    git clone git://github.com/CodeEffect/FooLime
+
+### Manually: ###
+Downoad a zip of the project (click on the zip icon further up the page) and extract
+it into your packages directory (`Preferences` / `Browse Packages…`).
+Go to the "Packages" directory (`Preferences` / `Browse Packages…`). Then clone this
+repository:
+
+## Default key bindings ##
+
+`f5` - Open the quick panel and browse for files and folders
+`shift+f5` - Control foobar: prev/next track, volume up/down, pause, quit
+
+## License ##
+
+FooLime is licensed under the MIT license.
+
+  Copyright (c) 2013 Steven Perfect <steve@codeeffect.co.uk>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ foolime.py
@@ -0,0 +1,228 @@
+import sublime
+import sublime_plugin
+import os
+import re
+import subprocess
+import time
+
+# Available switches:
+#   /add <list-of-files> - appends the specified files to the current playlist
+#       instead of replacing the playlist content and playing them immediately
+#   /immediate - bypasses the "please wait" dialog when adding files
+#   /play, /pause, /playpause, /prev, /next, /rand, /stop - playback controls
+#   /exit - exits foobar2000
+#   /show, /hide - shows or hides the main foobar2000 window
+#   /config - opens the Preferences dialog
+#   /command:<menu command> - invokes the specified main menu command
+#   /playlist_command:<context menu command> - invokes the specified context
+#       menu command on current playlist selection
+#   /playing_command:<context menu command> - invokes the specified context
+#       menu command on currently played track
+#   /context_command:<context menu command> <files> - invokes the specified
+#       context menu command on the specified files
+
+
+class FooLime(sublime_plugin.TextCommand):
+
+    settingFile = "FooLime.sublime-settings"
+    fooPath = "C:\\Program Files (x86)\\foobar2000\\foobar2000.exe"
+    settings = False
+    playing = False
+    folder = ""
+    items = []
+
+    def run(self, edit, action=None, alarmId=None):
+        action = str(action).lower()
+
+        if action == "none" or action == "choose":
+            self.selectTunes()
+        if action == "control":
+            self.control()
+
+    def selectTunes(self):
+        if not self.folder:
+            self.folder = self.getSettings().get(
+                "current_folder",
+                os.path.dirname(os.path.abspath(__file__))
+            )
+        if not self.folder or not os.path.isdir(self.folder):
+            self.folder = self.getSettings().get(
+                "home_folder",
+                os.path.dirname(os.path.abspath(__file__))
+            )
+        self.listMusic(self.folder)
+        self.addFolderOptions()
+        self.show_quick_panel(self.items, self.handleSelect)
+
+    def handleSelect(self, selection):
+        if selection is -1:
+            return
+
+        if selection is 0:
+            # Up a dir
+            if not os.path.isdir(self.folder[0:self.folder.rfind(os.sep)]):
+                return sublime.status_message("Unable to go up any further!")
+            self.folder = self.folder[0:self.folder.rfind(os.sep)]
+            self.getSettings().set("current_folder", self.folder)
+            sublime.save_settings(self.settingFile)
+            self.listMusic(self.folder)
+            self.addFolderOptions()
+            self.show_quick_panel(self.items, self.handleSelect)
+        elif selection is 1:
+            # play in order
+            self.playFile(self.folder)
+        elif selection is 2:
+            # play randomly
+            self.playFile(self.folder, random=True)
+        elif selection is 3:
+            # Home folder
+            self.folder = self.getSettings().get(
+                "home_folder",
+                os.path.dirname(os.path.abspath(__file__))
+            )
+            self.listMusic(self.folder)
+            self.addFolderOptions()
+            self.show_quick_panel(self.items, self.handleSelect)
+        else:
+            # a file / folder from self.items has been selected
+            selected = self.items[selection]
+            if selected[1] == "Folder":
+                self.folder = os.path.join(self.folder, selected[0])
+                self.getSettings().set("current_folder", self.folder)
+                sublime.save_settings(self.settingFile)
+                self.listMusic(self.folder)
+                self.addFolderOptions()
+                self.show_quick_panel(self.items, self.handleSelect)
+            else:
+                self.playFile(os.path.join(self.folder, selected[0]))
+
+    def control(self):
+        self.items = [
+            "Pause / play",
+            "Prev track",
+            "Next track",
+            "Quit",
+            "Volume up",
+            "Volume down",
+        ]
+        self.show_quick_panel(self.items, self.handleControl)
+
+    def handleControl(self, selection):
+        if selection is -1:
+            return
+
+        cmd = ""
+        selected = self.items[selection]
+
+        if selected == "Prev track":
+            cmd = "/prev"
+            self.items = [
+                "Prev track",
+                "Next track",
+                "Pause / play",
+                "Quit",
+                "Volume up",
+                "Volume down"
+            ]
+        elif selected == "Next track":
+            cmd = "/next"
+            self.items = [
+                "Next track",
+                "Prev track",
+                "Pause / play",
+                "Quit",
+                "Volume up",
+                "Volume down"
+            ]
+        elif selected == "Volume up":
+            cmd = "/command:Up"
+            self.items = [
+                "Volume up",
+                "Volume down",
+                "Pause / play",
+                "Prev track",
+                "Next track",
+                "Quit"
+            ]
+        elif selected == "Volume down":
+            cmd = "/command:Down"
+            self.items = [
+                "Volume down",
+                "Volume up",
+                "Pause / play",
+                "Prev track",
+                "Next track",
+                "Quit"
+            ]
+        elif selected == "Pause / play":
+            cmd = "/playpause"
+            self.items = []
+        elif selected == "Quit":
+            cmd = "/exit"
+            self.items = []
+
+        if cmd:
+            subprocess.Popen([
+                self.getSettings().get("foo_path", self.fooPath),
+                cmd
+            ])
+        if self.items:
+            self.show_quick_panel(self.items, self.handleControl)
+
+    def playFile(self, file, random=False):
+        self.playing = subprocess.Popen([
+            self.getSettings().get("foo_path", self.fooPath),
+            "/play",
+            file
+        ])
+        time.sleep(0.1)
+        subprocess.Popen([
+            self.getSettings().get("foo_path", self.fooPath),
+            "/hide"
+        ])
+        if random:
+            time.sleep(0.1)
+            subprocess.Popen([
+                self.getSettings().get("foo_path", self.fooPath),
+                "/rand"
+            ])
+
+    def listMusic(self, path):
+        if not path or not os.path.isdir(path):
+            path = self.getSettings().get(
+                "home_folder",
+                os.path.dirname(os.path.abspath(__file__))
+            )
+        if "%" in path:
+            path = os.path.expandvars(path)
+        self.items = []
+        self.folder = path
+        if self.folder[-1] == os.sep:
+            self.folder = self.folder[-1]
+        for f in os.listdir(path):
+            if os.path.isdir(os.path.join(path, f)):
+                self.items.append([f, "Folder"])
+            pattern = re.compile("(mp3|wav|ogg)$")
+            if pattern.search(f):
+                self.items.append([f, "%s file" % f[-3:]])
+
+    def addFolderOptions(self):
+        # Add other default options (play folder, play random)
+        self.items.insert(0, [
+            "Home folder",
+            str(self.getSettings().get("home_folder"))
+        ])
+        self.items.insert(0, ["Play folder randomly", self.folder])
+        self.items.insert(0, ["Play folder", self.folder])
+        self.items.insert(0, [".. Folder up", self.folder])
+
+    def getSettings(self):
+        if not self.settings:
+            self.settings = sublime.load_settings(self.settingFile)
+        return self.settings
+
+    def show_quick_panel(self, options, done):
+        sublime.set_timeout(
+            lambda: self.view.window().show_quick_panel(options, done),
+            10
+        )
